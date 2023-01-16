ac1954b1a29c36a683d58d7db67a036cc2626499
liwuchen <liwuchen@florens.com>
2023-01-16 15:32:24 +0800

    add: hash-object

--- ugit/cli.py
+++ ugit/cli.py
@@ -11,10 +11,18 @@ def parse_args():
     parser = argparse.ArgumentParser()
     commands = parser.add_subparsers(dest='command')
     commands.required = True
+    # init
     init_parser = commands.add_parser('init')
     init_parser.set_defaults(func=init)
+    # hash object
+    hash_object_parser = commands.add_parser('hash-object')
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument('file')
     return parser.parse_args()
+
 
 def init(args):
     data.init()
-
+def hash_object(args):
+    with open(args.file, 'rb') as f:
+        print(data.hash_object(f.read())) # print hash code
--- ugit/data.py
+++ ugit/data.py
@@ -1,7 +1,20 @@
 import os
+import hashlib
 
 GIT_DIR = '.ugit'
 
 def init():
-    os.makedirs(GIT_DIR)
-    print('Initialized empty ugit repository in %s' % os.path.join(os.getcwd(), GIT_DIR))
+    if os.path.exists(GIT_DIR):
+        # we do not really reinitialize git repository here
+        print('Reinitialized existing Git repository in %s' % os.path.join(os.getcwd(), GIT_DIR))
+    else:
+        os.makedirs(GIT_DIR)
+        os.makedirs(os.path.join(os.getcwd(), GIT_DIR, 'objects'))
+        print('Initialized empty ugit repository in %s' % os.path.join(os.getcwd(), GIT_DIR))
+
+def hash_object(data):
+    oid = hashlib.sha1(data).hexdigest() # hash object and convert to binary presentation
+    os.makedirs(os.path.join(os.getcwd(), GIT_DIR, 'objects', oid[:2]))
+    with open(os.path.join(os.getcwd(), GIT_DIR, 'objects', oid[:2], oid[2:]), 'wb') as out:
+        out.write(data)
+    return oid
